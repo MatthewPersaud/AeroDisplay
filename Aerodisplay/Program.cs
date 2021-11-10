70b91a93ca891271be6d573f6713d4e5578e257e
mason <mward7091@conestogac.on.ca>
2021-11-10 09:30:27 -0500

    Add project files.

---
+++ Aerodisplay/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aerodisplay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
