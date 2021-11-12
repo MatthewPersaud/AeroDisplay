a91a8b3edea57d5e22ad86006ad257c89054d7c3
mason <mward7091@conestogac.on.ca>
2021-11-12 11:47:11 -0500

    added AirDataComputer class, added altimeterdata.txt added ReadData()

---
+++ Aerodisplay/AirDataComputer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+
+namespace Aerodisplay
+{
+    class AirDataComputer
+    {
+        public static void ReadData()
+        {
+            string fileName = "altimeterdata.txt";
+            string path = Path.Combine(Environment.CurrentDirectory, @"Data\", fileName); 
+            //not sure if above works outside Debug mode
+
+            string[] lines = File.ReadAllLines(path);
+            /*
+            foreach (string r in lines)
+            {
+                Console.WriteLine("-- {0}", r);
+            }*/
+
+
+        }
+    }
+}
