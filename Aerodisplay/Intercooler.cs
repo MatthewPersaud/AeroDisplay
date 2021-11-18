3cc9b6cb3bb7a185b14fbefb23bb6ff851558b85
Jvuksic <94063818+Jvuksic@users.noreply.github.com>
2021-11-18 17:14:51 -0500

    Finished All 5 Devices

---
+++ Aerodisplay/Intercooler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pressure
+{
+    public class Intercooler
+    {
+        static private int desiredTemp;
+        static public void coolDown()//change the air temp to desired
+        {
+            int currentTemp = turboFanEngine.adjustPressure();
+            while (currentTemp > desiredTemp)
+            {
+                currentTemp--;
+            }
+            while (currentTemp < desiredTemp)
+            {
+                currentTemp++;
+            }
+        }
+        public static void changeDesTemp(int newTemp)//lets the pilot change the desired temp
+        {
+            desiredTemp = newTemp;
+        }
+    }
+}
---
+++ Aerodisplay/InterriorPressureSensors.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Pressure
+{
+    public class InterriorPressureSensors
+    {
+        static private float pressure;
+        static private string[] pressureMod;
+        static private int index;
+
+        static public float checkInteriorPressure()//basic check call
+        {
+            return pressure;
+        }
+
+        static public void setInteriorPressure(float newPressure)//set the pressure based off the enging
+        {
+            if (index <= 59)
+            {
+                index++;
+                pressure = newPressure * float.Parse(pressureMod[index - 1]); //apply random mod 
+            }
+            else
+            {
+                index = 0;
+                pressure = newPressure * float.Parse(pressureMod[index]);
+            }
+        }
+
+        static public void setPressureMod()//read from file
+        {
+            pressureMod = Controller.readFiles("PressureIn.txt");
+            index = 0;
+        }
+
+    }
+}
