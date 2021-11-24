d547fac9f36ffc2cff51eeaf4a98c4f74e8c9214
MatthewPersaud <Mpersaud4116@conestogac.on.ca>
2021-11-24 10:53:38 -0500

    Moved files

---
+++ Aerodisplay/RamPressureData.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Aerodisplay
+{
+    public class RamPressureData
+    {
+        int index = 0;
+
+        public int getIndex()
+        {
+            return index;
+        }
+
+        public void setIndex(int i)
+        {
+            index = i;
+        }
+
+        public static string[] readRamPressureDataFile()
+        {
+            string fileName = "DefaultRamPressureData.txt";
+
+            string[] lines = System.IO.File.ReadAllLines(fileName);
+
+            return lines;
+        }
+
+        public float getRamPressureData()
+        {
+            string[] data = readRamPressureDataFile();
+            int index = getIndex();
+            float ramPressureData = float.Parse(data[index]);
+
+            if (index == 49)
+                setIndex(0);
+
+            else
+                setIndex(index + 1);
+
+            return ramPressureData;
+        }
+    }
+}
