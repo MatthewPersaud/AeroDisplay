04494d3159c36117f6d4e29c8f066e5890ee8d47
mason <mward7091@conestogac.on.ca>
2021-11-24 16:19:28 -0500

    fixed namespaces

--- AerodisplayHMI/InterriorPressureSensors.cs
+++ AerodisplayHMI/InterriorPressureSensors.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 
 
-namespace Aerodisplay
+namespace AerodisplayHMI
 {
     public class InterriorPressureSensors
     {
--- AerodisplayHMI/OutflowValve.cs
+++ AerodisplayHMI/OutflowValve.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Aerodisplay
+namespace AerodisplayHMI
 {
     public class OutflowValve
     {
